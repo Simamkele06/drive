653ae0733f8139ff0431e802141d6cf36a102807
Simamkele06 <ST10479584@vcconnect.edu.za>
2025-09-03 14:53:51 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const age = 15;
+  let feedback ='';
+  const LicenseYears = 5;
+
+  if (age >= 18) 
+    if (LicenseYears >= 3)
+      {
+        feedback='you are a qualified , safe drive.👌'
+       }
+  else
+      {
+        feedback='you are allow to drive. YAY!😊'
+      }else 
+          {
+          feedback = 'You are not allowed to drive yet😩'
+         }
+    
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style ={styles.Response }>
+      {feedback} {/* This is my message to be displayed to the user */}
+       </Text>  
+    
     </View>
   );
 }
@@ -13,8 +37,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#BE2586',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  Response :{
+    fontSize : 28 ,
+    color: 'white',
+    fontWeight : 'bold',
+  } 
 });
