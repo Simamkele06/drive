5e3ad869b06ef71f6f5b21cca32b47e92b98bb21
Simamkele06 <ST10479584@vcconnect.edu.za>
2025-09-04 15:00:03 +0200

    Created image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
 
-  const age = 15;
+  const age = 20;
   let feedback ='';
   const LicenseYears = 5;
 
@@ -19,18 +19,29 @@ export default function App() {
           {
           feedback = 'You are not allowed to drive yet😩'
          }
-    
-
-
-
-
+  
   return (
-    <View style={styles.container}>
-    <Text style ={styles.Response }>
-      {feedback} {/* This is my message to be displayed to the user */}
-       </Text>  
+    <SafeAreaView style= {styles.container}> 
     
+     <View style= {styles.content}>
+      {/* add a costom style and other text component specific styles to the heading */}
+      <Text style ={[styles.Response, {fontSize:48,color:'black', marginBottom:35, textAlign:'center'}]}>Draving License Checker 
+      </Text>
+
+       {/*Our Image.*/}
+       <Image
+       source = {{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRixAoC2wuwU8X7ukgYb9obru3mSsgvm6rCSQ&s.jpeg', }}
+       style = {styles.image}/>
+       
+       <Text style ={styles.Response}>Age: {age}</Text>
+       <Text style ={styles.Response}>LicenseYears {LicenseYears}</Text>
+    <Text style ={[styles.Response ,{fontSize:48,color:'black', marginBottom:35, textAlign:'center'}]}>
+      {feedback} {/* This is my message to be displayed to the user */}
+     
+      </Text>
+     
     </View>
+    </SafeAreaView>
   );
 }
 
@@ -42,9 +53,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  Response :{
+    content: {
+       width : '100%',
+       alignItems :'center',
+
+    },
+
+   Response :{
     fontSize : 28 ,
     color: 'white',
     fontWeight : 'bold',
-  } 
+    marginVertical : 30
+    
+  },
+  
+   image:{
+    width: '100%',
+    height: '50%',
+    resizeMode:'cover',
+    top: 0,
+   },
 });
